9435d8b6e9dbe968c505c476d425e02741c91f9c
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-09-20 18:53:30 +0530

    LocationOfExpenditure as props

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,20 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 8, 12);
-    const expenseTitle = "Birthday Cake";
-    const expenseAmount = "Rs.1209";
-    const LocationOfExpenditure = "Bakery";
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2022, 8, 12);
+    // const expenseTitle = "Birthday Cake";
+    // const expenseAmount = "Rs.1209";
+    // const LocationOfExpenditure = "Bakery";
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            {/* <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
-            </div>
+            </div> */}
             <div className="expense-item__description">
-                <h2>{LocationOfExpenditure}</h2>
+                <h2>{props.location}</h2>
             </div>
-            <div className="expense-item__price">{expenseAmount}</div>
+            {/* <div className="expense-item__price">{expenseAmount}</div> */}
         </div>
     );
 }
