ac8d2fb561e0580fd21de8d167cf51fefc0543c1
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-09-21 16:25:58 +0530

    created ExpenseDetails as a separate component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,20 +1,15 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2022, 8, 12);
-    // const expenseTitle = "Birthday Cake";
-    // const expenseAmount = "Rs.1209";
-    // const LocationOfExpenditure = "Bakery";
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-            </div>
-            <div className="expense-item__description">
-                <h2>{props.location}</h2>
-            </div>
-            <div className="expense-item__price">{props.amount}</div>
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <ExpenseDetails
+                title={props.title}
+                amount={props.amount}
+            ></ExpenseDetails>
         </div>
     );
 }
