c4acec3ece29e2334cfafb2d96cae80d6831b686
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-09-21 16:56:40 +0530

    ..

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -5,11 +5,8 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <ExpenseDate date={props.date}></ExpenseDate>
-            <ExpenseDetails
-                title={props.title}
-                amount={props.amount}
-            ></ExpenseDetails>
+            <ExpenseDate date={props.date} />
+            <ExpenseDetails title={props.title} amount={props.amount} />
         </div>
     );
 }
