9c2b7002f61c16dd7fbb66b82118bdc56c3dc4e2
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-09-30 19:56:11 +0530

    Added a filter by year which does not work

--- src/Components/ExpenseDetails/Expenses.js
+++ src/Components/ExpenseDetails/Expenses.js
@@ -1,9 +1,20 @@
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState("2020");
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
     return (
         <Card className="expenses">
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
+            />
             {props.item.map((data) => (
                 <ExpenseItem
                     date={data.date}
